e1005bc1e1968970a92bc2752aa9b3994ee5b3e0
maruPortal <mariadelosangelesportal@gmail.com>
2020-10-10 18:46:19 -0300

    parte del new y update de user + arreglos

--- app/__init__.py
+++ app/__init__.py
@@ -50,6 +50,8 @@ def create_app(environment="development"):
     app.add_url_rule("/usuarios", "user_index", user.index)
     app.add_url_rule("/usuarios", "user_create", user.create, methods=["POST"])
     app.add_url_rule("/usuarios/nuevo", "user_new", user.new)
+#   app.add_url_rule("/usuarios/modificar", "user_update", user.update)
+#   app.add_url_rule("/usuarios", "user_", user., methods=["POST"])
 
     # Ruta para el Home (usando decorator)
     @app.route("/")
