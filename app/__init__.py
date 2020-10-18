500f49634a0e0f2b10752c41eb7530bb82dd804c
JoseMi Silva <js.silva.010@gmail.com>
2020-10-17 21:32:05 -0300

    fix del fix de los conflictos

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,15 @@
 from os import path, environ
-from flask import Flask, render_template, g, request, redirect, session, abort, url_for, flash
+from flask import (
+    Flask,
+    render_template,
+    g,
+    request,
+    redirect,
+    session,
+    abort,
+    url_for,
+    flash,
+)
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 from config import config
@@ -11,14 +21,11 @@ from app.helpers import handler
 from app.helpers import auth as helper_auth
 from app.models.modelos import initialize_db
 from app.resources.forms import RegistrationForm, LoginForm
-<<<<<<< HEAD
-from app.models.modelos import User
 
-=======
 # from app.db import connection
 from app.models.modelos import User
 from app.helpers.auth import authenticated
->>>>>>> 9fd387b49e318a7aaf83aad74a8d90fc15da9d11
+
 
 def create_app(environment="development"):
     # Configuración inicial de la app
@@ -33,13 +40,7 @@ def create_app(environment="development"):
     Session(app)
 
     # Configure db
-    app.config[
-        "SQLALCHEMY_DATABASE_URI"
-<<<<<<< HEAD
-    ] = "mysql+pymysql://root:password@172.17.0.4/entrega1"
-=======
-    ] = "mysql+pymysql://root:@localhost/proyecto"
->>>>>>> 9fd387b49e318a7aaf83aad74a8d90fc15da9d11
+    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost/proyecto"
     db = SQLAlchemy(app)
     """db.init_app(app)"""
     initialize_db(app)
@@ -83,14 +84,18 @@ def create_app(environment="development"):
     app.add_url_rule("/usuarios/show", "user_show", user.show)
 
     # app.add_url_rule("/usuarios/eliminar<int:id>", 'update_user', controlador_principal.update_user, methods=['GET'])
-    app.add_url_rule("/usuarios/eliminar<int:user_id>","user_eliminar", user.eliminar,methods=["GET"])
+    app.add_url_rule(
+        "/usuarios/eliminar<int:user_id>",
+        "user_eliminar",
+        user.eliminar,
+        methods=["GET"],
+    )
 
     app.add_url_rule("/usuarios", "user_create", user.create, methods=["POST"])
     # app.add_url_rule("/usuarios/nuevo", "user_new", user.new)
     # app.add_url_rule("/usuarios/modificar", "user_update", user.update)
     # app.add_url_rule("/usuarios", "user_", user., methods=["POST"])
 
-<<<<<<< HEAD
     @app.route("/usuarios/<int:user_id>/modificar", methods=["GET", "POST"])
     def update_user(user_id):
         user = User.query.get_or_404(user_id)
@@ -102,10 +107,6 @@ def create_app(environment="development"):
             # user.update(user, form)
             return redirect(url_for("user_index"))
         return render_template("user/update.html", form=form)
-=======
-    
-
->>>>>>> 9fd387b49e318a7aaf83aad74a8d90fc15da9d11
 
     @app.route("/usuarios/nuevo", methods=["GET", "POST"])
     def register():
@@ -125,7 +126,7 @@ def create_app(environment="development"):
         conn = SQLAlchemy()
         # conn = connection()
         us = User.all(conn)
-        return render_template("home.html",us=us)
+        return render_template("home.html", us=us)
 
     # Rutas de API-rest
     #    app.add_url_rule("/api/consultas", "api_issue_index", api_issue.index)
