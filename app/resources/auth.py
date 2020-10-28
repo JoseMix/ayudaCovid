7dd78640a20455c4430dcbfad8ed92b3f887ae35
maruPortal <mariadelosangelesportal@gmail.com>
2020-10-28 02:52:35 -0300

    sigue sin loguear, arregle estilos rotos con cambios anteriores

--- app/resources/auth.py
+++ app/resources/auth.py
@@ -11,16 +11,18 @@ def login():
     if form.validate_on_submit():
         user = User().find_by_email(form.email.data)
         if user and bcrypt.check_password_hash(user.password, form.password.data):
-            session["user"] = user["username"]
-            session["user_id"] = user["id"]
+            session['user'] = user['username']
+            session['user_id'] = user['id']
             flash("Usuario logueado correctamente")
             return redirect(url_for("home"))
-        else:    
+        else:
             flash("Usuario o Password incorrecto")
-    return render_template("auth/login.html", form=form)
+            return render_template("auth/login.html", form=form)
+    else:
+        return render_template("auth/login.html", form=form)
 
 def logout():
-    del session["user"]
+    del session['user']
     session.clear()
     flash("La sesión se cerró correctamente.")
     return redirect(url_for("home"))
