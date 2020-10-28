cfd9d175052bee44817923083daf529e5ca02aaa
JoseMi Silva <js.silva.010@gmail.com>
2020-10-28 16:47:34 -0300

    añadido print

--- app/resources/auth.py
+++ app/resources/auth.py
@@ -5,26 +5,30 @@ from app.models.configuracion import Configuracion
 from app.models.models import User
 from app.resources.forms import LoginForm
 
+
 def login():
     form = LoginForm()
     bcrypt = Bcrypt()
     if form.validate_on_submit():
         user = User().find_by_email(form.email.data)
         if user and bcrypt.check_password_hash(user.password, form.password.data):
-            session['user'] = user['username']
-            session['user_id'] = user['id']
+            session["user"] = user["username"]
+            session["user_id"] = user["id"]
             flash("Usuario logueado correctamente")
             return redirect(url_for("home"))
         else:
             flash("Usuario o Password incorrecto")
             return render_template("auth/login.html", form=form)
     else:
+        flash("pruebaaa")
+        print("lalalaal")
         return render_template("auth/login.html", form=form)
 
+
 def logout():
-    #del session['user']
-    session.pop('user',None)
-    session.pop('user_id',None)    
+    # del session['user']
+    session.pop("user", None)
+    session.pop("user_id", None)
     session.clear()
     flash("La sesión se cerró correctamente.")
     return redirect(url_for("home"))
