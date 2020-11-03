3d0460b3bd872dc61415d976e18cb83ef68d774e
maruPortal <mariadelosangelesportal@gmail.com>
2020-11-03 02:37:52 -0300

    credenciales para master

--- app/__init__.py
+++ app/__init__.py
@@ -46,7 +46,7 @@ def create_app(environment="development"):
     # Configure db
     app.config[
         "SQLALCHEMY_DATABASE_URI"
-    ] = "mysql+pymysql://maruca:maruca@localhost/proyecto"
+    ] = "mysql+pymysql://grupo13:NWE3YTMzYmU4YjY1@localhost/grupo13"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db = SQLAlchemy(app)
     initialize_db(app)
