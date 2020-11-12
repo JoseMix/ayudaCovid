062197400f2f2fc2a6dc61bc4f8ab5a5b332760b
JoseMi Silva <js.silva.010@gmail.com>
2020-11-12 15:45:12 -0300

    modificada ruta docker

--- app/__init__.py
+++ app/__init__.py
@@ -50,7 +50,7 @@ def create_app(environment="development"):
     # Configure db
     app.config[
         "SQLALCHEMY_DATABASE_URI"
-    ] = "mysql+pymysql://maruca:maruca@localhost/proyecto"
+    ] = "mysql+pymysql://root:password@172.17.0.4/grupo13"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db = SQLAlchemy(app)
     ma = Marshmallow(app)
