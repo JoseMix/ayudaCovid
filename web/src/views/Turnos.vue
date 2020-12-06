ef689f0f19a1468fe2b20535aa8fde769da8c66e
cata <rattocatalina@gmail.com>
2020-12-06 12:28:58 -0300

    turnos con vue, problemas con dar vuelta la fecha

--- web/src/views/Turnos.vue
+++ web/src/views/Turnos.vue
@@ -2,6 +2,17 @@
   <div class="Turnos">
     <h1>Página de Turnos</h1>
     <h2>{{ $route.params.nombre }}</h2>
+    <h2>{{ $route.params.fecha }}</h2>
+    <FormularioTurnos/>
     <router-link to="/centros" class="btn btn-primary">Volver </router-link>
   </div>
 </template>
+<script>
+import FormularioTurnos from "../components/TurnoComponents/FormularioTurnos.vue";
+export default {
+  name: "Turnos",
+  components: {
+    FormularioTurnos,
+  },
+};
+</script>
