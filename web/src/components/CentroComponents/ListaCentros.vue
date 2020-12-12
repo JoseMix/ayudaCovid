353182ba7f4e83a33fe426bf1db092b5e6ebff52
FlorenciaParedes <49081826+FlorenciaParedes@users.noreply.github.com>
2020-12-12 13:48:08 -0300

    fechas para hoy y 3 dias incompleto

--- web/src/components/CentroComponents/ListaCentros.vue
+++ web/src/components/CentroComponents/ListaCentros.vue
@@ -1,18 +1,20 @@
 <template>
   <v-container >
-    <v-layout row wrap  >
+    <v-layout row wrap  > {{ horarios() }} {{this.fecha_ini}} | {{this.fecha_fin}}
     <v-flex  xs12 sm6 >
     <v-card >
       <v-card-title>Centros</v-card-title>
       <v-card-text>
       <v-list v-for="centro in centros" :key="centro.id">    
-      <v-list-item>{{ centro.nombre }} | {{ centro.web }} </v-list-item>
+      <v-list-item>{{ centro.nombre }} | {{ centro.web }} |  </v-list-item>
       <v-list-item><FormulateForm v-model="formValues">
         <FormulateInput
           name="fecha"
           label="Fecha"
           type="date"
           validation="required"
+          min= "fecha_ini"
+          max= "fecha_fin" 
         />        
         <h3>{{ formValues.fecha }}</h3>
       </FormulateForm></v-list-item>
@@ -28,11 +30,24 @@
 </template>
 <script>
 import axios from "axios";
+import moment from "moment"; 
+//this.hora_fin = moment("2016-03-12 "+this.hora_inicio+":00").add(30, 'minutes').format('HH:mm');
 export default {
   data: () => ({
     centros: null,
     formValues: {},
+    fecha_fin:null,
+    fecha_ini:null,
   }),
+  methods:{
+    horarios(){
+      this.fecha_ini =  moment().format("YYYY[-]M[-]D"); 
+      console.log("hoy",this.fecha_ini);
+      this.fecha_fin = moment("2020-12-12"+ " 00:00:00").add(3, 'days').format("YYYY[-]M[-]D"); 
+      console.log("3 dias",this.fecha_fin);
+
+    }
+  },
   mounted: function() {
     //axios.get('http://127.0.0.1:5000/api/centros/').then(response =>console.log(response.data[0].centros[0][0].nombre))
     axios
