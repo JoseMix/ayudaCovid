e561f6fea67fc4d843cf88b02b7a0ff883568d83
cata <rattocatalina@gmail.com>
2020-12-15 20:18:39 -0300

    formulario y botones

--- web/src/components/TurnoComponents/FormularioTurnos.vue
+++ web/src/components/TurnoComponents/FormularioTurnos.vue
@@ -1,29 +1,29 @@
 <template>
   <div>
     <FormulateForm v-model="formValues" @submit="llamarAlOtroCOmponente">
-      <v-container fluid ma-0 px-6 fill-height>
-        <v-row justify="space-between">
-          <v-col md-6>
-              <h2>Formulario Turnos</h2>
-              <FormulateInput
-                name="hora"
-                label="Hora"
-                type="time"
-                validation="required"
-              />
-              <FormulateInput
-                name="email"
-                label="Email"
-                type="text"
-                validation="required"
-              />
-              <FormulateInput
-                name="telefono"
-                label="Telefono"
-                type="text"
-                validation="required"
-              />
-              <FormulateInput type="submit" label="Enviar" />
+      <v-container>
+        <v-row>
+          <v-col>
+            <h2>Formulario Turnos</h2>
+            <FormulateInput
+              name="hora"
+              label="Hora"
+              type="time"
+              validation="required"
+            />
+            <FormulateInput
+              name="email"
+              label="Email"
+              type="text"
+              validation="required"
+            />
+            <FormulateInput
+              name="telefono"
+              label="Telefono"
+              type="text"
+              validation="required"
+            />
+            <FormulateInput type="submit" label="Enviar" />
           </v-col>
         </v-row>
       </v-container>
