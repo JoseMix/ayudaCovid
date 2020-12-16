04257c056b5ec457bb04f20f907ae46c52e7ce0f
JoseMi Silva <js.silva.010@gmail.com>
2020-12-16 12:07:09 -0300

    fix api municipios

--- web/src/components/EstadisticasComponents/PieChart.vue
+++ web/src/components/EstadisticasComponents/PieChart.vue
@@ -64,7 +64,10 @@ export default {
   created() {
     axios
       .all([
-        axios.get("http://localhost:5000/api/municipios/top/5", {}),
+        axios.get(
+          "https://admin-grupo13.proyecto2020.linti.unlp.edu.ar/api/municipios/top/",
+          {}
+        ),
         axios.get(
           "https://api-referencias.proyecto2020.linti.unlp.edu.ar/municipios?page=1&per_page=140",
           {}
